228538c10cc42cec811de85be7190bac3dd8c4a0
rogerio75 <rogerio.ifg@gmail.com>
2023-10-02 13:04:57 -0300

    hoje 13:04

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//interface
+import { Tasklist } from '../../model/tasklist';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+public tasKList: Array<Tasklist>= [
+{task:"Minha Nova Tarefa", checked:true},
+{task:"Minha Nova Tarefa2", checked:false},
+
+
+];
+
+ngOnInit(): void {
+  
+}
 
 }
